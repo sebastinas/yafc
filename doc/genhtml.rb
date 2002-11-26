94b64e49f0b04d404269a6e5f854b222b7b05fa1
mhe <mhe>
2002-11-26 12:10:44 +0000

    convert to PHP code

--- doc/genhtml.rb
+++ doc/genhtml.rb
@@ -5,31 +5,28 @@
 
 def convert(filename)
   f = open(filename, "r");
-  ff = open(filename + ".2", "w");
+  fname = filename.gsub(".html", ".php")
+  ff = open(fname, "w");
 
-  printf(ff, "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"
- \"http://www.w3.org/TR/html4/strict.dtd\">\n");
+  printf(ff, "<?php
+ $title = 'manual :: %s';
+ $desc = 'yet another ftp client';
+ require('../header.php');
+?>\n", File.basename(fname).gsub(".php", "").gsub("-", " "));
 
+  in_body = false
+ 
   while !f.eof
 	 l = f.readline
-	 if l =~ /link href/
-		printf(ff, "%s", l);
-		printf(ff, "<link rel=\"stylesheet\" href=\"yafc.css\" type=\"text/css\">\n")
-	 elsif l =~ /<body>/
-		printf(ff, "%s", l);
-		printf(ff, "<div class=\"main\">\n");
-	 elsif l =~ /<\/body>/
-		printf(ff, "</div>\n");
-		printf(ff, "%s", l);
-	 else
-		printf(ff, "%s", l);
-	 end
+	 in_body = false if l =~ /<\/body>/
+	 printf(ff, "%s", l.gsub(".html", ".php")) if in_body
+	 in_body = true if l =~ /<body>/
   end
+
+  printf(ff, "<?php require('../footer.php');?>\n");
 	 
   f.close
   ff.close
-
-  File.rename(filename + ".2", filename);
 end
 
 dir = Dir.open("yafc/")
