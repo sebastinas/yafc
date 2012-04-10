914997b0073d71f918b139daf4b9ce57fc6fa9f3
Carles Pagès <page@cubata.homelinux.net>
2012-04-10 23:34:06 +0200

    Point to sebastinas fork.
    Update the README file to link there and remove all files to avoid people
    mistakenly using this.

--- lib/strcasecmp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STRCASECMP_H_INCLUDED
-#define _STRCASECMP_H_INCLUDED
-
-#include <stdio.h>  /* for size_t */
-
-int strcasecmp(const char *s1, const char *s2);
-int strncasecmp(const char *s1, const char *s2, size_t n);
-
-#endif
