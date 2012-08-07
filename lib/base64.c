d1c8f64f02d7843aeb4803f2aee6b483cc806e61
Sebastian Ramacher <sebastian+dev@ramacher.at>
2012-08-07 23:38:48 +0200

    Replace base64 implementation.
    We now use the base64 implementation from Heimdal if it is available or use
    OpenSSL to provide base64 support. This is necessary since the old base64 code
    was licensed as BSD-4-clause which is incompatible with the GPL-2+.

--- lib/base64.c
+++ lib/base64.c
@@ -1,150 +1,81 @@
 /*
- * Copyright (c) 1995, 1996, 1997 Kungliga Tekniska Högskolan
- * (Royal Institute of Technology, Stockholm, Sweden).
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Kungliga Tekniska
- *      Högskolan and its contributors.
- * 
- * 4. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * base64.c -- base64 support via OpenSSL
+ *
+ * Yet Another FTP Client
+ * Copyright (C) 2012, Sebastian Ramacher <sebastian+dev@ramacher.at>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. See COPYING for more details.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-#include <stdlib.h>
-#include <string.h>
 #include "base64.h"
 
-static char base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+#if defined(HAVE_BASE64_OPENSSL)
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <string.h>
 
-static int pos(char c)
+int base64_encode(const void* data, size_t size, char** str)
 {
-  char *p;
-  for(p = base64; *p; p++)
-    if(*p == c)
-      return p - base64;
-  return -1;
-}
+  if (!str || !data)
+    return -1;
 
-int base64_encode(const void *data, int size, char **str)
-{
-  char *s, *p;
-  int i;
-  int c;
-  unsigned char *q;
+  BIO* b64 = BIO_new(BIO_f_base64());
+  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
 
-  p = s = (char*)malloc(size*4/3+4);
-  if (p == NULL)
-      return -1;
-  q = (unsigned char*)data;
-  i=0;
-  for(i = 0; i < size;){
-    c=q[i++];
-    c*=256;
-    if(i < size)
-      c+=q[i];
-    i++;
-    c*=256;
-    if(i < size)
-      c+=q[i];
-    i++;
-    p[0]=base64[(c&0x00fc0000) >> 18];
-    p[1]=base64[(c&0x0003f000) >> 12];
-    p[2]=base64[(c&0x00000fc0) >> 6];
-    p[3]=base64[(c&0x0000003f) >> 0];
-    if(i > size)
-      p[3]='=';
-    if(i > size+1)
-      p[2]='=';
-    p+=4;
+  BIO* mem = BIO_new(BIO_s_mem());
+  b64 = BIO_push(b64, mem);
+
+  while (BIO_write(b64, data, size) <= 0)
+  {
+    if (BIO_should_retry(b64))
+      continue;
+    BIO_free_all(b64);
+    return -1;
   }
-  *p=0;
-  *str = s;
-  return strlen(s);
+  BIO_flush(b64);
+
+  char* tmp = NULL;
+  size_t len = BIO_get_mem_data(mem, &tmp);
+  *str = malloc(len);
+  if (!*str)
+  {
+    BIO_free_all(b64);
+    return -1;
+  }
+
+  strncpy(*str, tmp, len);
+  if (len > 0)
+    (*str)[len - 1] = '\0';
+
+  BIO_free_all(b64);
+  return len;
 }
 
-int base64_decode(const char *str, void *data)
+int base64_decode(const char* str, void* data)
 {
-  const char *p;
-  unsigned char *q;
-  int c;
-  int x;
-  int done = 0;
-  q=(unsigned char*)data;
-  for(p=str; *p && !done; p+=4){
-    x = pos(p[0]);
-    if(x >= 0)
-      c = x;
-    else{
-      done = 3;
-      break;
-    }
-    c*=64;
-    
-    x = pos(p[1]);
-    if(x >= 0)
-      c += x;
-    else
-      return -1;
-    c*=64;
-    
-    if(p[2] == '=')
-      done++;
-    else{
-      x = pos(p[2]);
-      if(x >= 0)
-	c += x;
-      else
-	return -1;
-    }
-    c*=64;
-    
-    if(p[3] == '=')
-      done++;
-    else{
-      if(done)
-	return -1;
-      x = pos(p[3]);
-      if(x >= 0)
-	c += x;
-      else
-	return -1;
-    }
-    if(done < 3)
-      *q++=(c&0x00ff0000)>>16;
-      
-    if(done < 2)
-      *q++=(c&0x0000ff00)>>8;
-    if(done < 1)
-      *q++=(c&0x000000ff)>>0;
+  if (!str || !data)
+    return -1;
+
+  BIO* b64 = BIO_new(BIO_f_base64());
+  BIO* mem = BIO_new_mem_buf((char*) str, -1);
+  b64 = BIO_push(b64, mem);
+
+  size_t len = strlen(str);
+  int res = 0;
+  while ((res = BIO_read(b64, data, len)) <= 0)
+  {
+    if (BIO_should_retry(b64))
+      continue;
+    BIO_free_all(b64);
+    return -1;
   }
-  return q - (unsigned char*)data;
+
+  BIO_free_all(b64);
+  return res;
 }
+#else
+#error no suitable base64 implementation available
+#endif
--- lib/base64.h
+++ lib/base64.h
@@ -1,46 +1,29 @@
 /*
- * Copyright (c) 1995, 1996, 1997 Kungliga Tekniska Högskolan
- * (Royal Institute of Technology, Stockholm, Sweden).
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Kungliga Tekniska
- *      Högskolan and its contributors.
- * 
- * 4. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * base64.h -- base64 support
+ *
+ * Yet Another FTP Client
+ * Copyright (C) 2012, Sebastian Ramacher <sebastian+dev@ramacher.at>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. See COPYING for more details.
  */
 
+#ifndef YAFC_BASE64_H
+#define YAFC_BASE64_H
 
-#ifndef _BASE64_H_
-#define _BASE64_H_
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 
-int base64_encode(const void *data, int size, char **str);
-int base64_decode(const char *str, void *data);
+#ifdef HAVE_BASE64_HEIMDAL
+#include <heimdal/base64.h>
+#else
+#include <stddef.h>
+
+int base64_encode(const void* data, size_t size, char** str);
+int base64_decode(const char* str, void* data);
+#endif
 
 #endif
