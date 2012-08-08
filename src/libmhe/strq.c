sebastinas/yafc

commit 3a1f7e77cd098e585b0617ff62f883ba8c7a649c

Fix invalid read in absolute_path.

src/libmhe/strq.c
@@ -259,7 +259,7 @@ char *path_absolute(const char *path, const char *curdir, const char *homedir)
 
 	if(path[0] == '~' && homedir)
 		p = tilde_expand_home(path, homedir);
-	else if(path[0] != '/' && path[1] != ':' && path[2] != '\\') {
+	else if(strlen(path) >= 3 && path[0] != '/' && path[1] != ':' && path[2] != '\\') {
 		if(strncmp(path, "./", 2) == 0)
 			asprintf(&p, "%s%s", curdir, path+1);
 		else

commit 0b0e38ac02056494e84543fc7888bd175a19c1fa

Fix regression introduced in 3a1f7e77cd098e585b0617ff62f883ba8c7a649c.

src/libmhe/strq.c
@@ -258,7 +258,7 @@ char *path_absolute(const char *path, const char *curdir, const char *homedir)
   char* p = NULL;
 	if(path[0] == '~' && homedir)
 		p = tilde_expand_home(path, homedir);
-	else if(strlen(path) >= 3 && path[0] != '/' && path[1] != ':' && path[2] != '\\') {
+	else if(strlen(path) < 3 || (path[0] != '/' && path[1] != ':' && path[2] != '\\')) {
 		if(strncmp(path, "./", 2) == 0)
     {
 			if (asprintf(&p, "%s%s", curdir, path+1) == -1)