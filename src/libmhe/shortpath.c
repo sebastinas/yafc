569160724099c406eaaef959b19aea3f5d4a5063
Celelibi <celelibi@gmail.com>
2015-04-08 19:58:24 +0200

    shortpath: simpler code
    Code shorter, simple, without loops, handles short directories better.
    Doesn't insert multiples "...".
    
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- src/libmhe/shortpath.c
+++ src/libmhe/shortpath.c
@@ -22,54 +22,35 @@
  * /usr/bin/averylongfilenamethatistoolongindeed.tar.gz => /...oolongindeed.tar.gz
  */
 
-static char* chop(const char *str, size_t maxlen)
-{
-	const size_t len = strlen(str);
-	if (len <= maxlen)
-		return xstrdup(str);
-
-	char* result = malloc(maxlen + 1);
-	strncpy(result, "...", 3);
-	strncpy(result + 3, str+(len-maxlen+3), maxlen - 3);
-	result[maxlen] = '\0';
-	return result;
-}
-
-static char* dirtodots(const char *path, size_t maxlen)
-{
-	const char* first_slash = strchr(path, '/');
-	if (!first_slash)
-		return chop(path, maxlen);
-
-	const char* end_slash = NULL;
-	if (strncmp(first_slash+1, "...", 3) == 0)
-		end_slash = strchr(first_slash+5, '/');
-	else
-		end_slash = strchr(first_slash+1, '/');
-
-	if (!end_slash)
-		return chop(path, maxlen);
-
-	size_t first = first_slash - path,
-				 end = end_slash - path;
-	char* result = xstrdup(path);
-	if (end - first < 4) /* /fu/ */
-		strpush(result + first +1, 4 - (end - first));
-	else /* /foobar/ */
-		strcpy(result + first + 4, end_slash);
-	strncpy(result + first + 1, "...", 3);
-	return result;
-}
-
 static char *_shortpath(char *path, size_t maxlen)
 {
-	const size_t len = strlen(path);
+	size_t len = strlen(path);
+	char *result = NULL;
+	const char *copy_from = NULL;
+	const char *first_slash = NULL;
+	size_t start_len = 0;
+
 	if (len <= maxlen)
 		return xstrdup(path);
 
-	char* tmp = dirtodots(path, maxlen);
-	char* result = _shortpath(tmp, maxlen);
-	free(tmp);
+	result = xmalloc(maxlen + 1);
+	result[0] = '\0';
+
+	first_slash = strchr(path, '/');
+	if (first_slash)
+		start_len = first_slash - path + 1;
+
+	if (maxlen - start_len - 3 > 0)
+		copy_from = strchr(path + len - (maxlen - start_len - 3), '/');
+
+	if (copy_from)
+		strncpy(result, path, start_len);
+	else
+		copy_from = path + len - (maxlen - 3);
+
+	strcat(result, "...");
+	strcat(result, copy_from);
+
 	return result;
 }
 
