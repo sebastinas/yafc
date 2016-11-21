f7272cd757517be2befe600c7566728c310f3d55
Sebastian Ramacher <sebastian+dev@ramacher.at>
2016-11-21 19:02:59 +0100

    Drop support for older readline versions
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- src/syshdr.h
+++ src/syshdr.h
@@ -135,8 +135,8 @@
 #    include <readline/readline.h>
 #  elif defined(HAVE_READLINE_H)
 #    include <readline.h>
-#  else /* !defined(HAVE_READLINE_H) */
-extern char *readline ();
+#  else
+#    error "readline without headers!"
 #  endif /* !defined(HAVE_READLINE_H) */
 #else /* !defined(HAVE_READLINE_READLINE_H) */
   /* no readline */
@@ -147,10 +147,8 @@ extern char *readline ();
 #    include <readline/history.h>
 #  elif defined(HAVE_HISTORY_H)
 #    include <history.h>
-#  else /* !defined(HAVE_HISTORY_H) */
-extern void add_history ();
-extern int write_history ();
-extern int read_history ();
+#  else
+#    error "readline without headers!"
 #  endif /* defined(HAVE_READLINE_HISTORY_H) */
   /* no history */
 #endif /* HAVE_READLINE_HISTORY */
@@ -158,10 +156,6 @@ extern int read_history ();
 #ifdef HAVE_LIBEDIT
 # include <editline/readline.h>
 # define HAVE_LIBREADLINE
-#endif
-
-#if defined(HAVE_LIBEDIT) || (defined(HAVE_LIBREADLINE) && RL_READLINE_VERSION < 0x0602)
-typedef CPPFunction rl_completion_func_t;
 #endif
 
 #ifdef HAVE_FNMATCH_GNU
